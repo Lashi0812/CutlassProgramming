9151be575f4f0a00759aacea4b0672fcc4f1da02
Lashi <lashi0812@gmail.com>
2023-10-28 15:03:32 +0000

    z order layout

--- csrc/layouts.cu
+++ csrc/layouts.cu
@@ -2,18 +2,27 @@
 #include <cute/layout.hpp>
 
 #include <iostream>
+using namespace cute;
 
 int main()
 {
-    auto shape4x3 = cute::make_shape(4, 3);
+    auto shape4x3 = make_shape(4, 3);
 
-    auto colMajorStride = cute::make_stride(1, 4);
-    auto colMajorLayout = cute::make_layout(shape4x3, colMajorStride);
+    auto colMajorStride = make_stride(1, 4);
+    auto colMajorLayout = make_layout(shape4x3, colMajorStride);
     std::cout << "Column Major Layout " << std::endl;
-    cute::print_layout(colMajorLayout);
+    print_layout(colMajorLayout);
 
-    auto rowMajorStride = cute::make_stride(3,1);
-    auto rowMajorLayout = cute::make_layout(shape4x3,rowMajorStride);
+    auto rowMajorStride = make_stride(3, 1);
+    auto rowMajorLayout = make_layout(shape4x3, rowMajorStride);
     std::cout << "Row Major Layout " << std::endl;
-    cute::print_layout(rowMajorLayout);
+    print_layout(rowMajorLayout);
+
+    auto shape8x8 = make_shape(make_shape(make_shape(2, 2), 2),
+                               make_shape(make_shape(2, 2), 2));
+    auto zOrderStride = make_stride(make_stride(make_stride(2, 8), 32),
+                                    make_stride(make_stride(1, 4), 16));
+    auto zOrderLayout = make_layout(shape8x8, zOrderStride);
+    std::cout << "Z order Layout " << std::endl;
+    print_layout(zOrderLayout);
 }
