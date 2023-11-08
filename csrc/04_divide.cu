fbc950de84979b682d2c153b2385f18b44da2867
Lashi <lashi0812@gmail.com>
2023-11-08 13:16:53 +0000

    tile vs shape in divide

--- csrc/04_divide.cu
+++ csrc/04_divide.cu
@@ -63,9 +63,32 @@ void test_single_tensor()
 	print("%d \n",a(1,1));
 }
 
+void test_tile_vs_shape()
+{
+	auto tile = make_tile(make_layout(_2{}), make_layout(_3{}));
+	auto shape = make_shape(_2{},_3{});
+	auto tensor_layout = make_layout(make_shape(_8{}, Int<15>{}));
+
+	auto tensor = make_counting_tensor(tensor_layout);
+	print("Tensor : ");
+	print_tensor(tensor);
+	print("\n");
+
+	auto zip_tile_divided = zipped_divide(tensor, tile);
+	print("Zipped Tiled Divided : ");
+	print_tensor(zip_tile_divided);
+	print("\n");
+
+	auto zip_shape_divided = zipped_divide(tensor, shape);
+	print("Zipped shape Divided : ");
+	print_tensor(zip_shape_divided);
+	print("\n");
+}
+
 int main()
 {
 	// test_logical_divide();
 	// test_zipped_divide_vs_logical_divide();
-	test_single_tensor();
+	// test_single_tensor();
+	test_tile_vs_shape();
 }
