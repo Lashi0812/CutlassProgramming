ad9e23cd1115b155affbc88b5e546f745b753059
Lashi <lashi0812@gmail.com>
2023-11-15 02:28:37 +0000

    manual copy

--- csrc/07_tensor_copy.cu
+++ csrc/07_tensor_copy.cu
@@ -45,8 +45,33 @@ void test_max_common_vec()
     copy(ten10, ten20);
 }
 
+void test_manual_copy()
+{
+    auto layout = Layout<Shape<_2, _2>, Stride<_2, _1>>{};
+
+    auto vec10 = std::vector{10, 11, 12, 13};
+    auto ten10 = make_tensor(vec10.data(), layout);
+
+    auto vec20 = std::vector{20, 21, 22, 23};
+    auto ten20 = make_tensor(vec20.data(), layout);
+
+    print("Before Copy: \n");
+    print_tensor(ten10);
+    print_tensor(ten20);
+
+    for (int i{0}; i < size(ten10); ++i)
+    {
+        ten10(i) = ten20(i);
+    }
+
+    print("After Copy: \n");
+    print_tensor(ten10);
+    print_tensor(ten20);
+}
+
 int main()
 {
     // test_copy();
-    test_max_common_vec();
+    // test_max_common_vec();
+    test_manual_copy();
 }
