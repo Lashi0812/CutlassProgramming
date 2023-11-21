6a9d89b858d05c35dac01f92b9d7ee4ea722b3d1
Lashi <lashi0812@gmail.com>
2023-11-21 06:35:39 +0000

    row col copy

--- csrc/07_tensor_copy.cu
+++ csrc/07_tensor_copy.cu
@@ -3,6 +3,7 @@
 #include <cute/algorithm/tuple_algorithms.hpp>
 #include <vector>
 #include <iostream>
+#include <numeric>
 
 using namespace cute;
 
@@ -69,9 +70,61 @@ void test_manual_copy()
     print_tensor(ten20);
 }
 
+template <typename SrcLayout, typename DstLayout>
+void test_copy(SrcLayout src_layout, DstLayout dst_layout, bool manual = false)
+{
+    std::vector<int> src_vec(size(src_layout));
+    std::vector<int> dst_vec(size(dst_layout));
+
+    std::iota(src_vec.begin(), src_vec.end(), 0);
+    auto src_tensor = make_tensor(src_vec.data(), src_layout);
+    auto dst_tensor = make_tensor(dst_vec.data(), dst_layout);
+
+    if (manual)
+    {
+        for (int i{0}; i < size(dst_layout); ++i)
+        {
+            dst_tensor(i) = src_tensor(i);
+        }
+    }
+    else
+    {
+        copy(src_tensor, dst_tensor);
+    }
+
+    print("Source Tensor : ");
+    print_tensor(src_tensor);
+    print("\n");
+
+    print("Destination Tensor : ");
+    print_tensor(dst_tensor);
+    print("\n");
+
+    print("Destination Tensor : ");
+    print_tensor(make_tensor(dst_vec.data(),make_layout(shape(dst_layout))));
+    print("\n");
+}
+
 int main()
 {
     // test_copy();
     // test_max_common_vec();
-    test_manual_copy();
+    // test_manual_copy();
+    {
+        // row row  copy
+        auto src_layout = make_layout(make_shape(2, 4));
+        auto dst_layout = make_layout(make_shape(4, 2));
+        test_copy(src_layout, dst_layout);
+    }
+    {
+        // row col copy
+        auto src_layout = make_layout(make_shape(2, 4));
+        auto dst_layout = make_layout(make_shape(4, 2),GenRowMajor());
+        test_copy(src_layout, dst_layout);
+    }
+    {
+        auto src_layout = make_layout(make_shape(2, 4));
+        auto dst_layout = make_layout(make_shape(4, 2),GenRowMajor());
+        test_copy(src_layout, dst_layout,true);
+    }
 }
