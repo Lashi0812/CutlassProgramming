988e42b10b952934407932768a94e6a1499045c0
Lashi <lashi0812@gmail.com>
2023-11-26 14:06:38 +0000

    transform fn

--- csrc/01d_layout_functions.cu
+++ csrc/01d_layout_functions.cu
@@ -22,7 +22,35 @@ void test_transform_leaf_examples()
     test_transform_leaf<tuple<_2, _m1, tuple<_m5, _1>>, abs_fn>();
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                              Transform
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename TupleA, typename TupleB, typename Fn>
+void test_transform()
+{
+    auto res = transform(TupleA{}, TupleB{}, Fn{});
+
+    // clang-format off
+    print("Input  : ");print(TupleA{});print(" , ");print(TupleB{});print("\n");
+    print("Output : ");print(res);print("\n");
+    // clang-format on
+}
+
+void test_transform_examples()
+{
+    {
+        print("Addition : \n");
+        test_transform<tuple<_2, _10>, tuple<_5, _6>, plus>();
+    }
+    {
+        print("Max  : \n");
+        test_transform<tuple<_2, _10>, tuple<_5, _6>, max_fn>();
+    }
+}
+
 int main()
 {
-    test_transform_leaf_examples();
+    // test_transform_leaf_examples();
+    test_transform_examples();
 }
