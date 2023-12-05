5cc6e2aebc87cbb15b18c80d8bf063fabd49ca52
Lashi <lashi0812@gmail.com>
2023-12-05 05:49:11 +0000

    tiled copy atom

--- csrc/15_understaing_copy.cu
+++ csrc/15_understaing_copy.cu
@@ -1,21 +1,23 @@
+#include "cute/numeric/int.hpp"
+#include "cute/util/print.hpp"
+#include "cutlass/half.h"
+#include "latex.hpp"
 #include <cute/algorithm/copy.hpp>
 
 using namespace cute;
 
 template <typename Args>
-void custom_print(Args args, int ps = -1)
-{
-    switch (ps)
-    {
-    case 0:
-        print_layout(args);
-        break;
-    case 1:
-        print_latex(args);
-        break;
-    default:
-        print(args);
-        break;
+void custom_print(Args args, int ps = -1) {
+    switch (ps) {
+        case 0:
+            print_layout(args);
+            break;
+        case 1:
+            print_latex(args);
+            break;
+        default:
+            print(args);
+            break;
     }
 }
 
@@ -24,8 +26,7 @@ void custom_print(Args args, int ps = -1)
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 template <typename S, typename D>
-void test_copy_trait()
-{
+void test_copy_trait() {
     using copy_trait = Copy_Traits<SM80_CP_ASYNC_CACHEALWAYS<S, D>>;
 
     print("Thread layout : ");
@@ -45,8 +46,7 @@ void test_copy_trait()
     print("\n");
 }
 
-void test_copy_trait_examples()
-{
+void test_copy_trait_examples() {
     {
         print("Int2 Bit Representation : \n");
         test_copy_trait<int2_t, int2_t>();
@@ -78,8 +78,7 @@ void test_copy_trait_examples()
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 template <typename S, typename D, typename T>
-void test_copy_atom()
-{
+void test_copy_atom() {
     using copy_atom = Copy_Atom<Copy_Traits<SM80_CP_ASYNC_CACHEALWAYS<S, D>>, T>;
 
     print("Bit Source Layout : \n");
@@ -104,17 +103,10 @@ void test_copy_atom()
     print("\n");
 }
 
-void test_copy_atom_examples()
-{
-    {
-        test_copy_atom<int, int, int8_t>();
-    }
-    {
-        test_copy_atom<uint128_t, uint128_t, half_t>();
-    }
-    {
-        test_copy_atom<uint128_t, uint128_t, int4_t>();
-    }
+void test_copy_atom_examples() {
+    { test_copy_atom<int, int, int8_t>(); }
+    { test_copy_atom<uint128_t, uint128_t, half_t>(); }
+    { test_copy_atom<uint128_t, uint128_t, int4_t>(); }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //                              End of Copy Atom
@@ -125,36 +117,53 @@ void test_copy_atom_examples()
 //  Produce the TiledCopy from the logical thread and Value layouts
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template <typename ThrLayout, typename ValLayout>
-void test_make_tiled_copy()
-{
-    auto res = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, half_t>{},
-                               ThrLayout{}, ValLayout{});
+template <
+  typename ThrLayout,
+  typename ValLayout,
+  typename Packed = uint128_t,
+  typename Original = half_t>
+void test_make_tiled_copy(char const *test_name = {}, int ps = 0) {
+    auto res = make_tiled_copy(
+      Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<Packed>, Original>{}, ThrLayout{}, ValLayout{});
 
-    print("Thread Layout : ");
-    print(ThrLayout{});
-    print("\n");
-    print("Value  Layout : ");
-    print(ValLayout{});
-    print("\n");
-    print(res);
+    if (ps == 1) {
+        print_latex(res, test_name);
+    } else {
+        print("Thread Layout : ");
+        print(ThrLayout{});
+        print("\n");
+        print("Value  Layout : ");
+        print(ValLayout{});
+        print("\n");
+        print(res);
+    }
 }
 
-void test_make_tiled_copy_examples()
-{
-    test_make_tiled_copy<Layout<Shape<_16, _8>, Stride<_8, _1>>,
-                         Layout<Shape<_1, _8>, Stride<_0, _1>>>();
+void test_make_tiled_copy_examples(int ps = 0) {
+    if (ps == 1)
+        print_latex_header();
+    test_make_tiled_copy<
+      Layout<Shape<_16, _8>, Stride<_8, _1>>,
+      Layout<Shape<_1, _8>, Stride<_0, _1>>>("CP_T16x8_V1x8", ps);
+
+    test_make_tiled_copy<Layout<Shape<_4, _8>>, Layout<Shape<_4, _2>>, uint64_t, half_t>(
+      "CP_T4x8_V4x2", ps);
+    if (ps == 1)
+        print_latex_footer();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //                              Understanding the function in Tiled Copy
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template <typename ThrLayout, typename ValLayout>
-void test_get_layouts()
-{
-    auto tiled_copy = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, half_t>{},
-                                      ThrLayout{}, ValLayout{});
+template <
+  typename ThrLayout,
+  typename ValLayout,
+  typename Packed = uint128_t,
+  typename Original = half_t>
+void test_get_layouts() {
+    auto tiled_copy = make_tiled_copy(
+      Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<Packed>, Original>{}, ThrLayout{}, ValLayout{});
 
     // clang-format off
     print("Thread Layout : ");print(ThrLayout{});print("\n");
@@ -167,17 +176,16 @@ void test_get_layouts()
     // clang-format on
 }
 
-void test_get_layouts_examples()
-{
-    test_get_layouts<Layout<Shape<_16, _8>, Stride<_8, _1>>,
-                     Layout<Shape<_1, _8>, Stride<_0, _1>>>();
+void test_get_layouts_examples() {
+    test_get_layouts<
+      Layout<Shape<_16, _8>, Stride<_8, _1>>,
+      Layout<Shape<_1, _8>, Stride<_0, _1>>>();
 }
 
 template <typename ThrLayout, typename ValLayout, typename SrcLayout>
-void test_tile2frag(int ps)
-{
-    auto tiled_copy = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, float>{},
-                                      ThrLayout{}, ValLayout{});
+void test_tile2frag(int ps) {
+    auto tiled_copy = make_tiled_copy(
+      Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, float>{}, ThrLayout{}, ValLayout{});
 
     auto thrFrag = tiled_copy.tidfrg_S(SrcLayout{});
 
@@ -190,18 +198,17 @@ void test_tile2frag(int ps)
     // clang-format on
 }
 
-void test_tile2frag_examples(int ps)
-{
-    test_tile2frag<Layout<Shape<_16, _8>, Stride<_8, _1>>,
-                   Layout<Shape<_1, _4>, Stride<_0, _1>>,
-                   Layout<Shape<_16, _64>>>(ps);
+void test_tile2frag_examples(int ps) {
+    test_tile2frag<
+      Layout<Shape<_16, _8>, Stride<_8, _1>>,
+      Layout<Shape<_1, _4>, Stride<_0, _1>>,
+      Layout<Shape<_16, _64>>>(ps);
 }
 
 template <typename ThrLayout, typename ValLayout, typename SrcLayout, int tidx>
-void test_get_slice(int ps)
-{
-    auto tiled_copy = make_tiled_copy(Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, float>{},
-                                      ThrLayout{}, ValLayout{});
+void test_get_slice(int ps) {
+    auto tiled_copy = make_tiled_copy(
+      Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, float>{}, ThrLayout{}, ValLayout{});
 
     auto tid2Frag = tiled_copy.tidfrg_S(SrcLayout{});
 
@@ -220,16 +227,15 @@ void test_get_slice(int ps)
     // clang-format on
 }
 
-void test_get_slice_examples(int ps)
-{
-    test_get_slice<Layout<Shape<_16, _8>, Stride<_8, _1>>,
-                   Layout<Shape<_1, _4>, Stride<_0, _1>>,
-                   Layout<Shape<_16, _64>>,
-                   0>(ps);
+void test_get_slice_examples(int ps) {
+    test_get_slice<
+      Layout<Shape<_16, _8>, Stride<_8, _1>>,
+      Layout<Shape<_1, _4>, Stride<_0, _1>>,
+      Layout<Shape<_16, _64>>,
+      0>(ps);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     // print_select
     [[maybe_unused]] int ps{-1};
     if (argc >= 2)
@@ -237,8 +243,8 @@ int main(int argc, char *argv[])
 
     // test_copy_trait_examples();
     // test_copy_atom_examples();
-    // test_make_tiled_copy_examples();
+    test_make_tiled_copy_examples(ps);
     // test_get_layouts_examples();
     // test_tile2frag_examples(ps);
-    test_get_slice_examples(ps);
+    // test_get_slice_examples(ps);
 }
