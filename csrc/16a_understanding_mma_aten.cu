b05d252147f3205c52de6df2c2049d5d97101af1
Lashi <lashi0812@gmail.com>
2023-12-14 10:07:17 +0000

    No Swizzle and thread Layout --> Bank conflict in Loading and Storing

--- csrc/16a_understanding_mma_aten.cu
+++ csrc/16a_understanding_mma_aten.cu
@@ -50,7 +50,7 @@ __global__ void kernel_mma(
     auto gB = make_tensor(make_gmem_ptr(B), layout_gB);
     auto gC = make_tensor(make_gmem_ptr(C), layout_gC);
 
-    auto sA = make_tensor(make_smem_ptr(storage.smem_a.data()), swi_layout_sA);
+    auto sA = make_tensor(make_smem_ptr(storage.smem_a.data()), layout_sA);
     auto sB = make_tensor(make_smem_ptr(storage.smem_b.data()), layout_sB);
 
     auto gs_thr_copy_A = gs_tiledCopy_A.get_thread_slice(threadIdx.x);
@@ -112,7 +112,7 @@ void host_mma() {
 
     auto gsTiledCopyA = make_tiled_copy(
       Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, half_t>{},
-      Layout<Shape<_16, _2>,Stride<_2,_1>>{},
+      Layout<Shape<_16, _2>>{},
       Layout<Shape<_1, _8>>{});
     auto gsTiledCopyB = make_tiled_copy(
       Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint64_t>, half_t>{},
