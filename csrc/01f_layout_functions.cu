cd7bb10443b552e0e7c1fa60b207aaf4c60ee2b5
Lashi <lashi0812@gmail.com>
2023-12-21 13:15:54 +0000

    tv layout in tiled copy

--- csrc/01f_layout_functions.cu
+++ csrc/01f_layout_functions.cu
@@ -1,4 +1,10 @@
 #include "cute/tensor.hpp"
+#include "cute/arch/copy_sm80.hpp"
+#include "cute/arch/mma_sm80.hpp"
+#include "cute/atom/copy_atom.hpp"
+#include "cute/numeric/int.hpp"
+#include "cute/numeric/integral_constant.hpp"
+#include "cute/numeric/tfloat.hpp"
 #include "latex.hpp"
 #include <cute/layout.hpp>
 #include <cute/algorithm/copy.hpp>
@@ -13,7 +19,7 @@ using namespace cute;
 
 template <typename MMA_Atom_OP_, typename Atom_Layout, typename Val_Layout, typename Copy_Atom_Op_>
 void test_sr_tiled_copy_and_tiled_mma(std::string test_name, int ps = -1) {
-    auto tiled_mma     = TiledMMA<MMA_Atom<MMA_Atom_OP_>, Atom_Layout, Val_Layout>{};
+    auto tiled_mma = TiledMMA<MMA_Atom<MMA_Atom_OP_>, Atom_Layout, Val_Layout>{};
 
     // A
     auto ivecA = std::vector<int>(128 * 32);
@@ -21,7 +27,7 @@ void test_sr_tiled_copy_and_tiled_mma(std::string test_name, int ps = -1) {
     auto smem_layoutA = tile_to_shape(
       composition(Swizzle<3, 2, 3>{}, Layout<Shape<_8, _32>, Stride<_32, _1>>{}),
       make_shape(_128{}, _32{}));
-    auto smem_tensorA = make_tensor(ivecA.data(), smem_layoutA);
+    auto smem_tensorA   = make_tensor(ivecA.data(), smem_layoutA);
     auto sr_tiled_copyA = make_tiled_copy_A(Copy_Atom<Copy_Atom_Op_, tfloat32_t>{}, tiled_mma);
 
     // B
@@ -30,7 +36,7 @@ void test_sr_tiled_copy_and_tiled_mma(std::string test_name, int ps = -1) {
     auto smem_layoutB = tile_to_shape(
       composition(Swizzle<3, 2, 3>{}, Layout<Shape<_8, _32>, Stride<_32, _1>>{}),
       make_shape(_128{}, _32{}));
-    auto smem_tensorB = make_tensor(ivecB.data(), smem_layoutB);
+    auto smem_tensorB   = make_tensor(ivecB.data(), smem_layoutB);
     auto sr_tiled_copyB = make_tiled_copy_B(Copy_Atom<Copy_Atom_Op_, tfloat32_t>{}, tiled_mma);
 
     if (ps == -1) {
@@ -105,10 +111,76 @@ void test_sr_tiled_copy_and_tiled_mma_examples(int ps = -1) {
         print_latex_footer();
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                          Understanding the TV layout in Tiled Copy
+
+// * TVLayout --> (thr,val)
+// * thr shape -->  this will be in the right inverse order
+// * (_32,_4):(_4,_1)
+// * ie. find the index of 1 (1)in the stride
+//              --> multiply the stride value (1) and shape value (4)at found index = A (4)
+// * Now find the index of A (0) in the stride
+//              --> multiply the stride value (4) and shape value (32) at found index = B (128)
+// * repeat
+// ? thr shape will be value at 1,0 in shape --> (4,32)
+
+// * val shape --> same as the val layout shape
+// * TVlayout stride  -->
+//      Start from val stride and mul val and stride put in thr stride
+
+// ThrLayout : (_32,_4):(_1,_32)
+// ValLayout : (_1,_4):(_0,_1)
+// TVLayout  : ((_32,_4),_4):((_1,_128),_32)
+//
+// ThrLayout : (_32,_4):(_1,_32)
+// ValLayout : (_2,_4):(_1,_2)
+// TVLayout  : ((_32,_4),(_2,_4)):((_2,_256),(_1,_64))
+//
+// ThrLayout : (_32,_4):(_4,_1)
+// ValLayout : (_1,_4):(_0,_1)
+// TVLayout  : ((_4,_32),_4):((_128,_1),_32)
+//
+// ThrLayout : (_32,_4):(_4,_1)
+// ValLayout : (_2,_4):(_1,_2)
+// TVLayout  : ((_4,_32),(_2,_4)):((_256,_2),(_1,_64))
+//
+// ThrLayout : (_32,_4):(_4,_1)
+// ValLayout : (_1,_8):(_0,_1)
+// TVLayout  : ((_4,_32),_8):((_256,_1),_32)
+//
+// ThrLayout : (_32,_4):(_4,_1)
+// ValLayout : (_2,_8):(_1,_2)
+// TVLayout  : ((_4,_32),(_2,_8)):((_512,_2),(_1,_64))
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename ThrLayout, typename ValLayout>
+void test_layout_tv() {
+    using tiled_copy = decltype(make_tiled_copy(
+      Copy_Atom<SM80_CP_ASYNC_CACHEALWAYS<uint128_t>, tfloat32_t>{}, ThrLayout{}, ValLayout{}));
+
+    // clang-format off
+    print("ThrLayout : ");print(ThrLayout{});print("\n");
+    print("ValLayout : ");print(ValLayout{});print("\n");
+    print("TVLayout  : ");print_layout(typename tiled_copy::TiledLayout_TV{});print("\n");
+    print("******************************************\n");
+    // clang-format on
+}
+
+void test_layout_tv_examples() {
+    test_layout_tv<Layout<Shape<_2, _4>, Stride<_1, _2>>, Layout<Shape<_1, _4>>>();
+    test_layout_tv<Layout<Shape<_2, _4>, Stride<_1, _2>>, Layout<Shape<_2, _4>>>();
+    test_layout_tv<Layout<Shape<_2, _4>, Stride<_4, _1>>, Layout<Shape<_1, _4>>>();
+    test_layout_tv<Layout<Shape<_2, _4>, Stride<_4, _1>>, Layout<Shape<_2, _4>>>();
+    test_layout_tv<Layout<Shape<_2, _4>, Stride<_4, _1>>, Layout<Shape<_1, _8>>>();
+    test_layout_tv<Layout<Shape<_2, _4>, Stride<_4, _1>>, Layout<Shape<_2, _8>>>();
+}
+
 int main(int argc, char *argv[]) {
     // print_select
     [[maybe_unused]] int ps{-1};
     if (argc >= 2)
         ps = atoi(argv[1]);
-    test_sr_tiled_copy_and_tiled_mma_examples(ps);
+    // test_sr_tiled_copy_and_tiled_mma_examples(ps);
+    test_layout_tv_examples();
 }
