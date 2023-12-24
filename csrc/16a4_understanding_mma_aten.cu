b808bfc6a1b7143bdf5f14c2d6ca2466ff3512d5
Lashi <lashi0812@gmail.com>
2023-12-24 12:16:34 +0000

    Mainloop --> kernel Operator --> Adapter

--- csrc/16a4_understanding_mma_aten.cu
+++ csrc/16a4_understanding_mma_aten.cu
@@ -22,6 +22,72 @@ constexpr int exponent(int result) {
     return exponent;
 }
 
+template <class IntT>
+Stride<IntT, Int<1>> make_cute_packed_stride(Stride<IntT, Int<1>> s, Shape<int, int> shape_MK) {
+    static_assert(
+      std::is_integral_v<IntT>,
+      "Stride must have an integral type so it can be set dynamically. Static strides not "
+      "supported.");
+    auto s_copy    = s;
+    get<0>(s_copy) = static_cast<IntT>(get<1>(shape_MK));
+    return s_copy;
+}
+
+template <class IntT>
+Stride<Int<1>, IntT> make_cute_packed_stride(Stride<Int<1>, IntT> s, Shape<int, int> shape_MK) {
+    static_assert(
+      std::is_integral_v<IntT>,
+      "Stride must have an integral type so it can be set dynamically. Static strides not "
+      "supported.");
+    auto s_copy    = s;
+    get<1>(s_copy) = static_cast<IntT>(get<0>(shape_MK));
+    return s_copy;
+}
+
+template <class L>
+struct TagToStrideA {
+    using type = L;
+};
+
+template <>
+struct TagToStrideA<GenRowMajor> {
+    using type = Stride<int32_t, Int<1>>;
+};
+
+// Maps to modes [M, K, L]
+template <>
+struct TagToStrideA<GenColMajor> {
+    using type = Stride<Int<1>, int32_t>;
+};
+
+template <class L>
+struct TagToStrideB {
+    using type = L;
+};
+
+template <>
+struct TagToStrideB<GenRowMajor> {
+    using type = Stride<Int<1>, int32_t>;
+};
+
+template <>
+struct TagToStrideB<GenColMajor> {
+    using type = Stride<int32_t, Int<1>>;
+};
+
+template <class LayoutTag>
+struct TagToStrideC : TagToStrideA<LayoutTag> {};
+
+// Convenience aliases
+template <class LayoutTag>
+using TagToStrideA_t = typename TagToStrideA<LayoutTag>::type;
+
+template <class LayoutTag>
+using TagToStrideB_t = typename TagToStrideB<LayoutTag>::type;
+
+template <class LayoutTag>
+using TagToStrideC_t = typename TagToStrideC<LayoutTag>::type;
+
 template <int I, class Shape>
 using get_t = decltype(get<I>(declval<Shape>()));
 
@@ -77,24 +143,39 @@ struct OperandB<T, sizeBK, GenColMajor, ThrCount> : OperandA<T, sizeBK, GenRowMa
 template <typename T, typename sizeBK, int ThrCount>
 struct OperandB<T, sizeBK, GenRowMajor, ThrCount> : OperandA<T, sizeBK, GenColMajor, ThrCount> {};
 
-template <typename T, typename sizeBK, typename MMA_Op_, typename AMajor, typename BMajor>
+template <
+  typename TAB,
+  typename TC_,
+  typename sizeBK,
+  typename MMA_Op_,
+  typename AMajor,
+  typename BMajor>
 struct GemmConfig {
+    using TA                      = TAB;
+    using TB                      = TAB;
+    using TC                      = TC_;
+    using StrideA                 = TagToStrideA_t<AMajor>;
+    using StrideB                 = TagToStrideB_t<BMajor>;
+    using StrideC                 = TagToStrideC_t<GenRowMajor>;
     using TileShape               = Shape<_128, _128, sizeBK>;
     static int constexpr ThrCount = 128;
-    using LdMatrixElemShapeMNK    = Shape<_16, _16, Int<(16 * 2) / sizeof(T)>>;
+    using LdMatrixElemShapeMNK    = Shape<_16, _16, Int<(16 * 2) / sizeof(TA)>>;
     using ValShape                = decltype(transform(
       LdMatrixElemShapeMNK{}, typename MMA_Traits<MMA_Op_>::Shape_MNK{}, divides{}));
     using TiledMMA = TiledMMA<MMA_Atom<MMA_Op_>, Layout<Shape<_2, _2, _1>>, Layout<ValShape>>;
 
-    using OperandA_ = OperandA<T, sizeBK, AMajor, ThrCount>;
-    using OperandB_ = OperandB<T, sizeBK, BMajor, ThrCount>;
+    using OperandA_ = OperandA<TA, sizeBK, AMajor, ThrCount>;
+    using OperandB_ = OperandB<TB, sizeBK, BMajor, ThrCount>;
 };
 
-template <typename TA_, typename TB_, typename TC_, typename Config>
-struct KernelOperator {
-    using TA             = TA_;
-    using TB             = TB_;
-    using TC             = TC_;
+template <typename Config>
+struct MainLoop {
+    using TA             = typename Config::TA;
+    using TB             = typename Config::TB;
+    using TC             = typename Config::TC;
+    using StrideA        = typename Config::StrideA;
+    using StrideB        = typename Config::StrideB;
+    using StrideC        = typename Config::StrideC;
     using TiledMMA_      = typename Config::TiledMMA;
     using TileShapeMNK   = typename Config::TileShape;
     using Layout_SA      = decltype(tile_to_shape(
@@ -110,45 +191,34 @@ struct KernelOperator {
     using SR_TiledCopy_B =
       decltype(make_tiled_copy_B(typename Config::OperandB_::SRCopyAtom{}, TiledMMA_{}));
 
-    KernelOperator() = default;
+    MainLoop() = default;
+
+    struct Params {
+        TA const *ptr_A;
+        TB const *ptr_B;
+        TC       *ptr_C;
+        StrideA   dA;
+        StrideB   dB;
+        StrideC   dC;
+    };
+    using Arguments = Params;
+
+    template <class ProblemShape>
+    static constexpr Params to_underlying_arguments(ProblemShape const &_, Arguments const &args) {
+        return args;
+    }
+
     struct SharedStorage {
         cute::array_aligned<TA, cute::cosize_v<Layout_SA>> smem_a;
         cute::array_aligned<TB, cute::cosize_v<Layout_SB>> smem_b;
     };
 
-    template <typename PShapeMNK>
-    CUTE_DEVICE void
-    operator()(TA const *A, TB const *B, TC *C, char *smem, PShapeMNK problemShapeMNK) {
+    template <typename TensorA, typename TensorB, typename TensorC>
+    CUTE_DEVICE void operator()(TensorA gA, TensorB gB, TensorC gC, char *smem) {
         SharedStorage &storage = *reinterpret_cast<SharedStorage *>(smem);
 
-        // auto mA = make_tensor(
-        //   make_gmem_ptr(A),
-        //   make_shape(get<0>(problemShapeMNK), get<2>(problemShapeMNK)),
-        //   Stride<int,_1>{});
-        // auto mB = make_tensor(
-        //   make_gmem_ptr(B),
-        //   make_shape(get<1>(problemShapeMNK), get<2>(problemShapeMNK)),
-        //   Stride<int,_1>{});
-        // auto mC = make_tensor(
-        //   make_gmem_ptr(C),
-        //   make_shape(get<0>(problemShapeMNK), get<1>(problemShapeMNK)),
-        //   Stride<int,_1>{});
-
-        auto mA = make_tensor(make_gmem_ptr(A), Layout<Shape<_128, _16>, Stride<_16, _1>>{});
-        auto mB = make_tensor(make_gmem_ptr(B), Layout<Shape<_128, _16>, Stride<_16, _1>>{});
-        auto mC = make_tensor(make_gmem_ptr(C), Layout<Shape<_128, _128>, Stride<_128, _1>>{});
-
-        auto sA = make_tensor(make_smem_ptr(storage.smem_a.data()), Layout_SA{});
-        auto sB = make_tensor(make_smem_ptr(storage.smem_b.data()), Layout_SB{});
-
-        auto blk_shape                   = TileShapeMNK{};
-        auto [m_coord, n_coord, l_coord] = static_cast<uint3>(blockIdx);
-        auto blk_coord                   = make_coord(m_coord, n_coord, _);
-
-        auto gA = local_tile(mA, blk_shape, blk_coord, Step<_1, X, _1>{});
-        auto gB = local_tile(mB, blk_shape, blk_coord, Step<X, _1, _1>{});
-        auto gC = local_tile(mC, blk_shape, blk_coord, Step<_1, _1, X>{});
-
+        auto           sA = make_tensor(make_smem_ptr(storage.smem_a.data()), Layout_SA{});
+        auto           sB = make_tensor(make_smem_ptr(storage.smem_b.data()), Layout_SB{});
         GS_TiledCopy_A gs_tiledCopy_A;
         auto           gs_thr_copy_A = gs_tiledCopy_A.get_thread_slice(threadIdx.x);
         auto           tAgA          = gs_thr_copy_A.partition_S(gA);
@@ -182,6 +252,7 @@ struct KernelOperator {
             copy(gs_tiledCopy_B, tBgB(_, _, _, 0), tBsB);
             cp_async_fence();
             cp_async_wait<0>();
+            __syncthreads();
 
             // auto ptr1 = &(tCrA(0));
             copy(sr_tiledCopy_A, tCsA, tCrA_view);
@@ -190,34 +261,112 @@ struct KernelOperator {
             gemm(tiledMAA, fragC, tCrA, tCrB, fragC);
         }
 
-        copy(fragC, tCrC);
+        copy_aligned(fragC, tCrC);
     }
 };
 
-template <typename Operator, typename TA, typename TB, typename TC, typename PShapeMNK>
-__global__ void kernel_mma(TA const *A, TB const *B, TC *C, PShapeMNK problemShapeMNK)
-// clang-format on
-{
+template <typename ProblemShapeMNK_, typename MainLoop_>
+struct KernelOperator {
+    using ProblemShapeMNK = ProblemShapeMNK_;
+    using MainLoop        = MainLoop_;
+    using MainLoopParams  = typename MainLoop_::Params;
+    using TileShapeMNK    = typename MainLoop_::TileShapeMNK;
+
+    KernelOperator() = default;
+    struct Params {
+        ProblemShapeMNK problem_shape;
+        MainLoopParams  mainLoop;
+    };
+
+    using Arguments = Params;
+
+    static Params to_underlying_arguments(Arguments const &args) {
+        return {
+          args.problem_shape, MainLoop::to_underlying_arguments(args.problem_shape, args.mainLoop)};
+    }
+
+    CUTE_DEVICE void operator()(Params params, char *smem) {
+        auto mA = make_tensor(
+          make_gmem_ptr(params.mainLoop.ptr_A),
+          make_shape(get<0>(params.problem_shape), get<2>(params.problem_shape)),
+          params.mainLoop.dA);
+        auto mB = make_tensor(
+          make_gmem_ptr(params.mainLoop.ptr_B),
+          make_shape(get<1>(params.problem_shape), get<2>(params.problem_shape)),
+          params.mainLoop.dB);
+        auto mC = make_tensor(
+          make_gmem_ptr(params.mainLoop.ptr_C),
+          make_shape(get<0>(params.problem_shape), get<1>(params.problem_shape)),
+          params.mainLoop.dC);
+
+        auto blk_shape                   = TileShapeMNK{};
+        auto [m_coord, n_coord, l_coord] = static_cast<uint3>(blockIdx);
+        auto blk_coord                   = make_coord(m_coord, n_coord, _);
+
+        auto gA = local_tile(mA, blk_shape, blk_coord, Step<_1, X, _1>{});
+        auto gB = local_tile(mB, blk_shape, blk_coord, Step<X, _1, _1>{});
+        auto gC = local_tile(mC, blk_shape, blk_coord, Step<_1, _1, X>{});
+
+        MainLoop mainLoop;
+        mainLoop(gA, gB, gC, smem);
+    }
+};
+
+template <typename Operator>
+__global__ void kernel_mma(typename Operator::Params params) {
     extern __shared__ char smem[];
     Operator               op;
-    op(A, B, C, smem, problemShapeMNK);
+    op(params, smem);
 }
+
+template <typename Kernel_>
+struct KernelAdapter {
+    using Operator        = Kernel_;
+    using Loop            = typename Operator::MainLoop;
+    using Params_         = typename Operator::Params;
+    using ProblemShapeMNK = typename Operator::ProblemShapeMNK;
+    using TA              = typename Loop::TA;
+    using TB              = typename Loop::TB;
+    using TC              = typename Loop::TC;
+    using StrideA         = typename Loop::StrideA;
+    using StrideB         = typename Loop::StrideB;
+    using StrideC         = typename Loop::StrideC;
+
+    StrideA strideA;
+    StrideB strideB;
+    StrideC strideC;
+
+    Params_ params_;
+
+    void initialize(ProblemShapeMNK problem_size, TA const *d_A, TB const *d_B, TC *d_C) {
+
+        auto M = cute::size<0>(problem_size);
+        auto N = cute::size<1>(problem_size);
+        auto K = cute::size<2>(problem_size);
+
+        strideA = make_cute_packed_stride(StrideA{}, make_shape(M, K));
+        strideB = make_cute_packed_stride(StrideB{}, make_shape(N, K));
+        strideC = make_cute_packed_stride(StrideC{}, make_shape(M, N));
+
+        auto args =
+          typename Operator::Arguments{problem_size, {d_A, d_B, d_C, strideA, strideB, strideC}};
+
+        params_ = Operator::to_underlying_arguments(args);
+    }
+
+    void run(ProblemShapeMNK problem_size, TA const *d_A, TB const *d_B, TC *d_C) {
+        initialize(problem_size, d_A, d_B, d_C);
+        static int constexpr SHARED_SIZE = sizeof(typename Loop::SharedStorage{});
+        kernel_mma<Operator><<<1, 128, SHARED_SIZE>>>(params_);
+    }
+};
 
 template <typename GEMM_Config_>
 void host_mma() {
 
-    // auto h_A = at::arange(
-    //              decltype(get<0>(layout_gA) * get<1>(layout_gA))::value,
-    //              at::TensorOptions().dtype(at::kFloat))
-    //              .reshape({get<0>(layout_gA), get<1>(layout_gA)});
-    // auto h_B = at::arange(
-    //              decltype(get<0>(layout_gB) * get<1>(layout_gB))::value,
-    //              at::TensorOptions().dtype(at::kFloat))
-    //              .reshape({get<0>(layout_gB), get<1>(layout_gB)});
-
-    int M = 128;
-    int N = 128;
-    int K = 16;
+    auto M = 128;
+    auto N = 128;
+    auto K = 16;
 
     auto problemShapeMNK = make_shape(M, N, K);
 
@@ -236,11 +385,14 @@ void host_mma() {
     cudaMemcpy(d_A, h_A.data_ptr(), h_A.numel() * h_A.element_size(), cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B.data_ptr(), h_B.numel() * h_B.element_size(), cudaMemcpyHostToDevice);
 
-    using Operator = KernelOperator<tfloat32_t, tfloat32_t, float, GEMM_Config_>;
+    using Loop     = MainLoop<GEMM_Config_>;
+    using Operator = KernelOperator<Shape<int, int, int>, Loop>;
+    using Adapter  = KernelAdapter<Operator>;
 
-    static int constexpr SHARED_SIZE = sizeof(typename Operator::SharedStorage{});
+    Adapter ap;
+    ap.run(problemShapeMNK, d_A, d_B, d_C);
+    cudaDeviceSynchronize();
 
-    kernel_mma<Operator><<<1, 128, SHARED_SIZE>>>(d_A, d_B, d_C, problemShapeMNK);
     cudaMemcpy(h_C.data_ptr(), d_C, h_C.numel() * h_C.element_size(), cudaMemcpyDeviceToHost);
 
     // auto cpu_ans = at::zeros_like(c);
@@ -259,9 +411,17 @@ void host_mma() {
     // std::cout << cpu_ans << std::endl;
     // std::cout << h_C << std::endl;
     // std::cout << (h_C.isclose(cpu_ans, 1e-02)) << std::endl;
+    cudaFree(d_A);
+    cudaFree(d_B);
+    cudaFree(d_C);
 }
 
 int main() {
-    host_mma<
-      GemmConfig<tfloat32_t, _16, SM80_16x8x8_F32TF32TF32F32_TN, GenRowMajor, GenColMajor>>();
+    host_mma<GemmConfig<
+      tfloat32_t,
+      float,
+      _16,
+      SM80_16x8x8_F32TF32TF32F32_TN,
+      GenRowMajor,
+      GenColMajor>>();
 }
