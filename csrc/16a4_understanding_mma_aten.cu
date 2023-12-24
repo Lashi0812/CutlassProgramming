17affbf3368f9f3ddd81d49662ccc1c9537c56e1
Lashi <lashi0812@gmail.com>
2023-12-24 06:37:53 +0000

    Using kernel Operator and predefine the problem shape

--- csrc/16a4_understanding_mma_aten.cu
+++ csrc/16a4_understanding_mma_aten.cu
@@ -1,3 +1,5 @@
+#include "cute/config.hpp"
+#include "cute/numeric/integral_constant.hpp"
 #include "cute/stride.hpp"
 #include "cute/tensor.hpp"
 #include "cute/arch/copy_sm75.hpp"
@@ -5,9 +7,11 @@
 #include "cute/atom/copy_atom.hpp"
 #include "cute/layout.hpp"
 #include "cute/numeric/tfloat.hpp"
+#include "cute/underscore.hpp"
 #include <ATen/ATen.h>
 
 using namespace cute;
+using X = Underscore;
 
 constexpr int exponent(int result) {
     int exponent = 0;
@@ -86,152 +90,121 @@ struct GemmConfig {
     using OperandB_ = OperandB<T, sizeBK, BMajor, ThrCount>;
 };
 
-template <
-  typename TA,
-  typename TB,
-  typename TC,
-  typename Layout_GA,
-  typename Layout_GB,
-  typename Layout_GC,
-  typename Layout_SA,
-  typename Layout_SB,
-  typename GS_TiledCopy_A,
-  typename GS_TiledCopy_B,
-  typename SR_TiledCopy_A,
-  typename SR_TiledCopy_B,
-  typename TiledMMA_>
-__global__ void kernel_mma(
-  TA const      *A,
-  TB const      *B,
-  TC            *C,
-  Layout_GA      layout_gA,
-  Layout_GB      layout_gB,
-  Layout_GC      layout_gC,
-  Layout_SA      layout_sA,
-  Layout_SB      layout_sB,
-  GS_TiledCopy_A gs_tiledCopy_A,
-  GS_TiledCopy_B gs_tiledCopy_B,
-  SR_TiledCopy_A sr_tiledCopy_A,
-  SR_TiledCopy_B sr_tiledCopy_B,
-  TiledMMA_      tiledMAA)
-// clang-format on
-{
-    extern __shared__ char smem[];
+template <typename TA_, typename TB_, typename TC_, typename Config>
+struct KernelOperator {
+    using TA             = TA_;
+    using TB             = TB_;
+    using TC             = TC_;
+    using TiledMMA_      = typename Config::TiledMMA;
+    using TileShapeMNK   = typename Config::TileShape;
+    using Layout_SA      = decltype(tile_to_shape(
+      typename Config::OperandA_::SmemLayout{},
+      make_shape(get<0>(TileShapeMNK{}), get<2>(TileShapeMNK{}))));
+    using Layout_SB      = decltype(tile_to_shape(
+      typename Config::OperandB_::SmemLayout{},
+      make_shape(get<1>(TileShapeMNK{}), get<2>(TileShapeMNK{}))));
+    using GS_TiledCopy_A = typename Config::OperandA_::GSTiledCopy;
+    using GS_TiledCopy_B = typename Config::OperandB_::GSTiledCopy;
+    using SR_TiledCopy_A =
+      decltype(make_tiled_copy_A(typename Config::OperandA_::SRCopyAtom{}, TiledMMA_{}));
+    using SR_TiledCopy_B =
+      decltype(make_tiled_copy_B(typename Config::OperandB_::SRCopyAtom{}, TiledMMA_{}));
 
+    KernelOperator() = default;
     struct SharedStorage {
         cute::array_aligned<TA, cute::cosize_v<Layout_SA>> smem_a;
         cute::array_aligned<TB, cute::cosize_v<Layout_SB>> smem_b;
     };
 
-    SharedStorage &storage = *reinterpret_cast<SharedStorage *>(smem);
+    template <typename PShapeMNK>
+    CUTE_DEVICE void
+    operator()(TA const *A, TB const *B, TC *C, char *smem, PShapeMNK problemShapeMNK) {
+        SharedStorage &storage = *reinterpret_cast<SharedStorage *>(smem);
 
-    auto gA = make_tensor(make_gmem_ptr(A), layout_gA);
-    auto gB = make_tensor(make_gmem_ptr(B), layout_gB);
-    auto gC = make_tensor(make_gmem_ptr(C), layout_gC);
+        // auto mA = make_tensor(
+        //   make_gmem_ptr(A),
+        //   make_shape(get<0>(problemShapeMNK), get<2>(problemShapeMNK)),
+        //   Stride<int,_1>{});
+        // auto mB = make_tensor(
+        //   make_gmem_ptr(B),
+        //   make_shape(get<1>(problemShapeMNK), get<2>(problemShapeMNK)),
+        //   Stride<int,_1>{});
+        // auto mC = make_tensor(
+        //   make_gmem_ptr(C),
+        //   make_shape(get<0>(problemShapeMNK), get<1>(problemShapeMNK)),
+        //   Stride<int,_1>{});
 
-    auto sA = make_tensor(make_smem_ptr(storage.smem_a.data()), layout_sA);
-    auto sB = make_tensor(make_smem_ptr(storage.smem_b.data()), layout_sB);
+        auto mA = make_tensor(make_gmem_ptr(A), Layout<Shape<_128, _16>, Stride<_16, _1>>{});
+        auto mB = make_tensor(make_gmem_ptr(B), Layout<Shape<_128, _16>, Stride<_16, _1>>{});
+        auto mC = make_tensor(make_gmem_ptr(C), Layout<Shape<_128, _128>, Stride<_128, _1>>{});
 
-    auto gs_thr_copy_A = gs_tiledCopy_A.get_thread_slice(threadIdx.x);
-    auto tAgA          = gs_thr_copy_A.partition_S(gA);
-    auto tAsA          = gs_thr_copy_A.partition_D(sA);
+        auto sA = make_tensor(make_smem_ptr(storage.smem_a.data()), Layout_SA{});
+        auto sB = make_tensor(make_smem_ptr(storage.smem_b.data()), Layout_SB{});
 
-    auto gs_thr_copy_B = gs_tiledCopy_B.get_thread_slice(threadIdx.x);
-    auto tBgB          = gs_thr_copy_B.partition_S(gB);
-    auto tBsB          = gs_thr_copy_B.partition_D(sB);
+        auto blk_shape                   = TileShapeMNK{};
+        auto [m_coord, n_coord, l_coord] = static_cast<uint3>(blockIdx);
+        auto blk_coord                   = make_coord(m_coord, n_coord, _);
 
-    copy(gs_tiledCopy_A, tAgA, tAsA);
-    copy(gs_tiledCopy_B, tBgB, tBsB);
-    cp_async_fence();
-    cp_async_wait<0>();
+        auto gA = local_tile(mA, blk_shape, blk_coord, Step<_1, X, _1>{});
+        auto gB = local_tile(mB, blk_shape, blk_coord, Step<X, _1, _1>{});
+        auto gC = local_tile(mC, blk_shape, blk_coord, Step<_1, _1, X>{});
 
-    auto thr_mma = tiledMAA.get_thread_slice(threadIdx.x);
-    auto tCrA    = thr_mma.partition_fragment_A(sA);
-    auto tCrB    = thr_mma.partition_fragment_B(sB);
-    auto tCrC    = thr_mma.partition_C(gC);
+        GS_TiledCopy_A gs_tiledCopy_A;
+        auto           gs_thr_copy_A = gs_tiledCopy_A.get_thread_slice(threadIdx.x);
+        auto           tAgA          = gs_thr_copy_A.partition_S(gA);
+        auto           tAsA          = gs_thr_copy_A.partition_D(sA);
 
-    auto sr_thr_copy_A = sr_tiledCopy_A.get_thread_slice(threadIdx.x);
-    auto tCsA          = sr_thr_copy_A.partition_S(sA);
-    auto tCrA_view     = sr_thr_copy_A.retile_D(tCrA);
+        GS_TiledCopy_B gs_tiledCopy_B;
+        auto           gs_thr_copy_B = gs_tiledCopy_B.get_thread_slice(threadIdx.x);
+        auto           tBgB          = gs_thr_copy_B.partition_S(gB);
+        auto           tBsB          = gs_thr_copy_B.partition_D(sB);
 
-    auto sr_thr_copy_B = sr_tiledCopy_B.get_thread_slice(threadIdx.x);
-    auto tCsB          = sr_thr_copy_B.partition_S(sB);
-    auto tCrB_view     = sr_thr_copy_B.retile_D(tCrB);
+        TiledMMA_ tiledMAA;
+        auto      thr_mma = tiledMAA.get_thread_slice(threadIdx.x);
+        auto      tCrA    = thr_mma.partition_fragment_A(sA);
+        auto      tCrB    = thr_mma.partition_fragment_B(sB);
+        auto      tCrC    = thr_mma.partition_C(gC);
 
-    auto ptr1 = &(tCrA(0));
-    copy(sr_tiledCopy_A, tCsA, tCrA_view);
-    copy(sr_tiledCopy_B, tCsB, tCrB_view);
+        SR_TiledCopy_A sr_tiledCopy_A;
+        auto           sr_thr_copy_A = sr_tiledCopy_A.get_thread_slice(threadIdx.x);
+        auto           tCsA          = sr_thr_copy_A.partition_S(sA);
+        auto           tCrA_view     = sr_thr_copy_A.retile_D(tCrA);
+        SR_TiledCopy_B sr_tiledCopy_B;
+        auto           sr_thr_copy_B = sr_tiledCopy_B.get_thread_slice(threadIdx.x);
+        auto           tCsB          = sr_thr_copy_B.partition_S(sB);
+        auto           tCrB_view     = sr_thr_copy_B.retile_D(tCrB);
 
-    auto fragC = partition_fragment_C(tiledMAA, shape(gC));
-    clear(fragC);
+        auto fragC = partition_fragment_C(tiledMAA, take<0, 2>(TileShapeMNK{}));
+        clear(fragC);
 
-    if (thread0()) {
-        // clang-format off
-        print("gA            : ");print(gA           );print("\n");
-        print("gB            : ");print(gB           );print("\n");
-        print("gC            : ");print(gC           );print("\n");
+        for (int k_tile_iter{0}; k_tile_iter < size<2>(gA); ++k_tile_iter) {
+            copy(gs_tiledCopy_A, tAgA(_, _, _, 0), tAsA);
+            copy(gs_tiledCopy_B, tBgB(_, _, _, 0), tBsB);
+            cp_async_fence();
+            cp_async_wait<0>();
 
-        print("sA            : ");print(sA           );print("\n");
-        print("sB            : ");print(sB           );print("\n");
-        
-        print("gs_thr_copy_A : ");print(gs_thr_copy_A);print("\n");
-        print("tAgA          : ");print(tAgA         );print("\n");
-        print("tAsA          : ");print(tAsA         );print("\n");
-        print("gs_thr_copy_B : ");print(gs_thr_copy_B);print("\n");
-        print("tBgB          : ");print(tBgB         );print("\n");
-        print("tBsB          : ");print(tBsB         );print("\n");
-        print("thr_mma       : ");print(thr_mma      );print("\n");
-        print("tCrA          : ");print(tCrA         );print("\n");
-        print("tCrB          : ");print(tCrB         );print("\n");
-        print("tCrC          : ");print(tCrC         );print("\n");
-        print("sr_thr_copy_A : ");print(sr_thr_copy_A);print("\n");
-        print("tCsA          : ");print(tCsA         );print("\n");
-        print("tCrA_view     : ");print(tCrA_view    );print("\n");
-        print("sr_thr_copy_B : ");print(sr_thr_copy_B);print("\n");
-        print("tCsB          : ");print(tCsB         );print("\n");
-        print("tCrB_view     : ");print(tCrB_view    );print("\n");
-        print("fragC         : ");print(fragC        );print("\n");
-        // clang-format off
+            // auto ptr1 = &(tCrA(0));
+            copy(sr_tiledCopy_A, tCsA, tCrA_view);
+            copy(sr_tiledCopy_B, tCsB, tCrB_view);
+
+            gemm(tiledMAA, fragC, tCrA, tCrB, fragC);
+        }
+
+        copy(fragC, tCrC);
     }
+};
 
-    gemm(tiledMAA, fragC, tCrA, tCrB, fragC);
-    copy(fragC, tCrC);
+template <typename Operator, typename TA, typename TB, typename TC, typename PShapeMNK>
+__global__ void kernel_mma(TA const *A, TB const *B, TC *C, PShapeMNK problemShapeMNK)
+// clang-format on
+{
+    extern __shared__ char smem[];
+    Operator               op;
+    op(A, B, C, smem, problemShapeMNK);
 }
 
 template <typename GEMM_Config_>
 void host_mma() {
-
-    using AConfig         = typename GEMM_Config_::OperandA_;
-    using BConfig         = typename GEMM_Config_::OperandB_;
-    using ProblemShapeMNK = typename GEMM_Config_::TileShape;
-
-    auto layout_gA = Layout<
-      Shape<get_t<0, ProblemShapeMNK>, get_t<2, ProblemShapeMNK>>,
-      Stride<get_t<2, ProblemShapeMNK>, _1>>{};
-    auto layout_gB = Layout<
-      Shape<get_t<1, ProblemShapeMNK>, get_t<2, ProblemShapeMNK>>,
-      Stride<get_t<2, ProblemShapeMNK>, _1>>{};
-    auto layout_gC = Layout<
-      Shape<get_t<0, ProblemShapeMNK>, get_t<1, ProblemShapeMNK>>,
-      Stride<get_t<1, ProblemShapeMNK>, _1>>{};
-
-    auto layout_sA = tile_to_shape(typename AConfig::SmemLayout{}, shape(layout_gA));
-    auto layout_sB = tile_to_shape(typename BConfig::SmemLayout{}, shape(layout_gB));
-
-    struct SharedStorage {
-        cute::array_aligned<tfloat32_t, cute::cosize_v<decltype(layout_sA)>> smem_a;
-        cute::array_aligned<tfloat32_t, cute::cosize_v<decltype(layout_sB)>> smem_b;
-    };
-
-    static constexpr int SHARED_SIZE = static_cast<int>(sizeof(SharedStorage));
-
-    auto tiled_mma = typename GEMM_Config_::TiledMMA{};
-
-    auto gsTiledCopyA = typename AConfig::GSTiledCopy{};
-    auto gsTiledCopyB = typename BConfig::GSTiledCopy{};
-
-    auto srTiledCopyA = make_tiled_copy_A(typename AConfig::SRCopyAtom{}, tiled_mma);
-    auto srTiledCopyB = make_tiled_copy_B(typename BConfig::SRCopyAtom{}, tiled_mma);
 
     // auto h_A = at::arange(
     //              decltype(get<0>(layout_gA) * get<1>(layout_gA))::value,
@@ -242,14 +215,17 @@ void host_mma() {
     //              at::TensorOptions().dtype(at::kFloat))
     //              .reshape({get<0>(layout_gB), get<1>(layout_gB)});
 
-    auto h_A =
-      at::rand({size<0>(layout_gA), size<1>(layout_gA)}, at::TensorOptions().dtype(at::kFloat));
+    int M = 128;
+    int N = 128;
+    int K = 16;
 
-    auto h_B =
-      at::rand({size<0>(layout_gB), size<1>(layout_gB)}, at::TensorOptions().dtype(at::kFloat));
+    auto problemShapeMNK = make_shape(M, N, K);
 
-    auto h_C =
-      at::zeros({size<0>(layout_gC), size<1>(layout_gC)}, at::TensorOptions().dtype(at::kFloat));
+    auto h_A = at::rand({M, K}, at::TensorOptions().dtype(at::kFloat));
+
+    auto h_B = at::rand({N, K}, at::TensorOptions().dtype(at::kFloat));
+
+    auto h_C = at::zeros({M, N}, at::TensorOptions().dtype(at::kFloat));
 
     tfloat32_t *d_A, *d_B;
     float      *d_C;
@@ -260,20 +236,11 @@ void host_mma() {
     cudaMemcpy(d_A, h_A.data_ptr(), h_A.numel() * h_A.element_size(), cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B.data_ptr(), h_B.numel() * h_B.element_size(), cudaMemcpyHostToDevice);
 
-    kernel_mma<<<1, 128, SHARED_SIZE>>>(
-      d_A,
-      d_B,
-      d_C,
-      layout_gA,
-      layout_gB,
-      layout_gC,
-      layout_sA,
-      layout_sB,
-      gsTiledCopyA,
-      gsTiledCopyB,
-      srTiledCopyA,
-      srTiledCopyB,
-      tiled_mma);
+    using Operator = KernelOperator<tfloat32_t, tfloat32_t, float, GEMM_Config_>;
+
+    static int constexpr SHARED_SIZE = sizeof(typename Operator::SharedStorage{});
+
+    kernel_mma<Operator><<<1, 128, SHARED_SIZE>>>(d_A, d_B, d_C, problemShapeMNK);
     cudaMemcpy(h_C.data_ptr(), d_C, h_C.numel() * h_C.element_size(), cudaMemcpyDeviceToHost);
 
     // auto cpu_ans = at::zeros_like(c);
@@ -294,4 +261,7 @@ void host_mma() {
     // std::cout << (h_C.isclose(cpu_ans, 1e-02)) << std::endl;
 }
 
-int main() { host_mma<GemmConfig<tfloat32_t, _16, SM80_16x8x8_F32TF32TF32F32_TN,GenRowMajor,GenColMajor>>(); }
+int main() {
+    host_mma<
+      GemmConfig<tfloat32_t, _16, SM80_16x8x8_F32TF32TF32F32_TN, GenRowMajor, GenColMajor>>();
+}
